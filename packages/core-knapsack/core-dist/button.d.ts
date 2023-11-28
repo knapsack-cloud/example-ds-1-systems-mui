150c093033210c9ee2556343912777ea24a6756b
Brittany Smart <30271981+brittanysmart@users.noreply.github.com>
2023-11-28 13:01:04 -0600

    Untrack all files in core-dist directory

--- packages/core-knapsack/core-dist/button.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface ButtonProps {
-    children: React.ReactNode;
-    type?: 'primary' | 'secondary';
-    size?: 'small' | 'medium' | 'large';
-}
-export declare const Button: React.FC<ButtonProps>;
-export {};
