208a5df51eb8f4539f8b7f05a89f79bdcba294b7
Brittany Smart <30271981+brittanysmart@users.noreply.github.com>
2024-02-13 14:07:42 -0600

    Add custom page patterns and tokens Add custom pages for contact us and contribute Add custom page for community Add custom page for foundations Add custom page for get started Update design tokens for primary and secondary colors Update design tokens for primary and secondary colors in brand-1 Update demo with primary color and medium size button Update demo with primary color and medium size button in brand-1 Update design tokens for primary color in brand-1 Update demo with primary color and medium size button in brand-1 Update pattern link Update pattern list Update pattern avatar Update pattern card actions

--- packages/brand-1/brand-1-theme.js
+++ packages/brand-1/brand-1-theme.js
@@ -1,6 +1,19 @@
 import { createTheme } from '@mui/material/styles';
+// import { colorPrimary, colorSecondary } from '@my-org/core-tokens';
+import {
+  colorPrimary,
+  colorSecondary,
+} from '../brand-1-knapsack/dist/tokens/design-tokens.json';
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: colorPrimary,
+    },
+    secondary: {
+      main: colorSecondary,
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {
--- packages/core/core-theme.js
+++ packages/core/core-theme.js
@@ -1,18 +1,27 @@
 import { createTheme } from '@mui/material/styles';
-// import { colorPrimary, colorSecondary } from '@my-org/core-tokens';
+import { colorPrimary, colorSecondary } from '@my-org/core-tokens';
+
 const theme = createTheme({
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        containedPrimary: {
-          backgroundColor: 'red',
-          '&:hover': {
-            backgroundColor: 'purple',
-          },
-        },
-      },
+  palette: {
+    primary: {
+      main: colorPrimary,
+    },
+    secondary: {
+      main: colorSecondary,
     },
   },
+  // components: {
+  //   MuiButton: {
+  //     styleOverrides: {
+  //       containedPrimary: {
+  //         backgroundColor: colorPrimary,
+  //         '&:hover': {
+  //           backgroundColor: colorSecondary,
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
 });
 
 export default theme;
